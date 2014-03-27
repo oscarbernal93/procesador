800f9543a6666b5f81325dc86d46364dd0915665
Oscar Bernal <oebernal@utp.edu.co>
2014-03-26 19:08:53 -0500

    Añadida funcionalidad del Register File

--- RF.vhd
+++ RF.vhd
@@ -9,14 +9,39 @@ entity RF is
            rd : in  STD_LOGIC_VECTOR (4 downto 0);
            dwr : in  STD_LOGIC_VECTOR (31 downto 0);
            rst : in  STD_LOGIC;
+			  clk : in STD_LOGIC;
            CRs1 : out  STD_LOGIC_VECTOR (31 downto 0);
            CRs2 : out  STD_LOGIC_VECTOR (31 downto 0));
 end RF;
 
 architecture Behavioral of RF is
 
+type rf_rom is array (0 to 31) of std_logic_vector (31 downto 0);
+	impure function InitRomRf return rf_rom is
+		variable var_rf : rf_rom;
+		begin
+		for I in rf_rom'range loop
+			var_rf(i) := "00000000000000000000000000000000";	
+		end loop;
+		return var_rf;
+	end function;
+signal mem_rf : rf_rom := InitRomRf;
 begin
+process
+begin
+		IF RST = '1' THEN
+			CRs1 <= "00000000000000000000000000000000";
+			CRs2 <= "00000000000000000000000000000000";
+		ELSE 
+			IF rising_edge(clk) THEN
+				CRs1 <= mem_rf( conv_integer(rs1) );
+				CRs2 <= mem_rf( conv_integer(rs2) );
+				IF rd /= "00000" THEN
+					mem_rf(conv_integer(rd)) <= dwr;
+				END IF;
+			END IF;
+		END IF;
+end process;
 
 
 end Behavioral;
-
