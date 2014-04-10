076cc7e62a4d4d8ffde417a952a8cac694dc311c
Oscar Bernal <oebernal@utp.edu.co>
2014-04-10 08:09:59 -0500

    añadido reset al PSR

--- psr.vhd
+++ psr.vhd
@@ -6,18 +6,23 @@ use IEEE.NUMERIC_STD.ALL;
 entity psr is
     Port ( nzvc : in  STD_LOGIC_VECTOR (3 downto 0);
 			  CLK : in STD_LOGIC;
+			  RST : in STD_LOGIC;
            carry : out  STD_LOGIC);
 end psr;
 
 architecture Behavioral of psr is
 SIGNAL rpsr : std_logic_vector (31 downto 0) := "00000000000000000000000000000000";
 begin
-process (rpsr,nzvc,CLK)
+process (rpsr,nzvc,CLK, RST)
 	begin	
+	if rst then
+		carry <= '0';
+	else
 		IF rising_edge(CLK) THEN
 			rpsr (23 downto 20) <= nzvc;
 			carry <= rpsr (20);
 		END IF;
+	end if;
 	end process;
 end Behavioral;
 
